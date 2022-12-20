7916a449438340f9e5fce182808c7aa02114792a
hhoangcpascal <havanhoang2911@gmail.com>
2022-12-20 11:18:00 +0700

    Update 2

--- SE104-QLVMB/DTO/UserGroup.cs
+++ SE104-QLVMB/DTO/UserGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace SE104_QLVMB.DTO
 {
     internal class UserGroup
     {
+        public UserGroup(DataRow dr)
+        {
+            ma_nhom = dr["MaNhom"].ToString();
+            ten_nhom = dr["TenNhom"].ToString();
+        }
+
+        private string ma_nhom, ten_nhom;
+
+        public string MaNhom { get { return ma_nhom; } set { ma_nhom = value; } }
+        public string TenNhom { get { return ten_nhom; } set { ten_nhom = value; } }
     }
 }
